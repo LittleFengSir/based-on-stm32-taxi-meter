8a1e33a48bf01d69a574b25433dcb5d711ddafce
小冯Sir <fengsir.hy@foxmail.com>
2024-05-03 03:32:02 +0800

    add key control

--- Firmware/Core/apps/src/key.c
+++ Firmware/Core/apps/src/key.c
@@ -1,11 +1,23 @@
-#include <stdlib.h>
 #include "key.h"
-#include "tim.h"
-#include "stdbool.h"
-
-
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-
+  if (GPIO_Pin == KEY1_Pin) {
+	if (HAL_GPIO_ReadPin(KEY1_GPIO_Port, KEY1_Pin) == GPIO_PIN_SET) {
+	  while (HAL_GPIO_ReadPin(KEY1_GPIO_Port, KEY1_Pin) == GPIO_PIN_SET);
+	  keyVal = 1;
+	}
+  }
+  if (GPIO_Pin == KEY2_Pin) {
+	if (HAL_GPIO_ReadPin(KEY2_GPIO_Port, KEY2_Pin) == GPIO_PIN_RESET) {
+	  while (HAL_GPIO_ReadPin(KEY2_GPIO_Port, KEY2_Pin) == GPIO_PIN_RESET);
+	  keyVal = 2;
+	}
+  }
+  if (GPIO_Pin == KEY3_Pin) {
+	if (HAL_GPIO_ReadPin(KEY3_GPIO_Port, KEY3_Pin) == GPIO_PIN_RESET) {
+	  while (HAL_GPIO_ReadPin(KEY3_GPIO_Port, KEY3_Pin) == GPIO_PIN_RESET);
+	  keyVal = 3;
+	}
+  }
 }
 
